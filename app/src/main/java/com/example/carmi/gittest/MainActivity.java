7b10c077250c49e38c9dc0a81501c279f813e814
Deku <han17007@byui.edu>
2018-10-31 14:52:19 -0600

    adding a string

--- app/src/main/java/com/example/carmi/gittest/MainActivity.java
+++ app/src/main/java/com/example/carmi/gittest/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         String og = "Orignal Gangster";
+        String b = "bubbles";
     }
 }
