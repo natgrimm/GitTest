cc4c0ef37a53d09e3a033a9b05d3b8166922946a
Deku <han17007@byui.edu>
2018-11-13 17:38:39 -0700

    Changed line in "settings.gradle" to be "include ':app'" instead of "include ':library', 'sample'". This was causing a lot of problems with the Android Studio setup.

--- app/src/main/java/com/example/carmi/gittest/MainActivity.java
+++ app/src/main/java/com/example/carmi/gittest/MainActivity.java
@@ -14,6 +14,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         List<Appointment> schedule = new LinkedList<Appointment>();
-
     }
 }
