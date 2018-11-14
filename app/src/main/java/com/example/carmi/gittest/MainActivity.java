e16aa82f43e3ce09ce3d6d5f13c153e36770f408
natgrimm <natrgrim@gmail.com>
2018-11-14 15:00:48 -0700

    Worked on shared preferences for name.

--- app/src/main/java/com/example/carmi/gittest/MainActivity.java
+++ app/src/main/java/com/example/carmi/gittest/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.carmi.gittest;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.widget.EditText;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -13,6 +17,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        if (preferences != null) {
+            String name = preferences.getString("Name", null);
+
+            if (name != null) {
+                EditText tb = findViewById(R.id.textName);
+                tb.setText(name);
+            }
+        }
+
+
         List<Appointment> schedule = new LinkedList<Appointment>();
+    }
+
+    @Override
+    protected void onStop() {
+
+        super.onStop();
+
+        SharedPreferences spref = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor prefEditor = spref.edit();
+        EditText tb = findViewById(R.id.textName);
+        String name = String.valueOf(tb.getText());
+        prefEditor.putString("Name", name);
+        prefEditor.apply();
+
     }
 }
--- app/src/test/java/com/example/carmi/gittest/PhoneCorrectTest.java
+++ app/src/test/java/com/example/carmi/gittest/PhoneCorrectTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.assertEquals;
 public class PhoneCorrectTest {
     @Test
     public void doesPhoneHaveTenDigits(){
-        Appointment a = new Appointment();
+        Appointment a = new Appointment(0,0,0,0);
         assertEquals(true, a.checkPhone("2082063868"));
     }
 }
