68146a9757eb489696ba58490256127238c9be24
Deku <han17007@byui.edu>
2018-12-11 20:00:18 -0700

    Added Firebase database dependency

--- app/src/main/java/com/example/carmi/gittest/CreateAppointment.java
+++ app/src/main/java/com/example/carmi/gittest/CreateAppointment.java
@@ -2,6 +2,7 @@ package com.example.carmi.gittest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CreateAppointment extends AppCompatActivity {
 
@@ -9,5 +10,17 @@ public class CreateAppointment extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_appointment);
+    }
+
+    public void createAppointment(View v) {
+        /*
+        DatabaseReference postsRef = ref.child("posts");
+
+        DatabaseReference newPostRef = postsRef.push();
+        newPostRef.setValueAsync(new Post("gracehop", "Announcing COBOL, a New Programming Language"));
+
+        // We can also chain the two calls together
+        postsRef.push().setValueAsync(new Post("alanisawesome", "The Turing Machine"));
+        */
     }
 }
